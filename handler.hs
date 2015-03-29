3bda7452dfc1532805e5ee9cae6f9e911c5e9840
Dmitry Tomp <dmitrytomp@gmail.com>
2015-03-29 10:51:32 +0300

    Will add desktop removal later

--- handler.hs
+++ handler.hs
@@ -23,6 +23,28 @@ getWorkspaceName (UserState wIdx wList) =
     let Workspace wName dIdx dList = wList !! wIdx
     in getDesktopName wName (dList !! dIdx)
 
+modInd :: [a] -> Int -> a -> [a]
+modInd list idx newElem = let (pref, suff) = splitAt idx list in pref ++ newElem : tail suff
+
+readUserState :: IO UserState
+readUserState = do
+    let parseInput (':':c:name)
+            | toLower c `elem` "of" =
+                let (desktop, cont) = span (/= ':') name
+                in (desktop, isUpper c) : parseInput cont
+            | otherwise = parseInput name
+        parseInput (_:s) = parseInput s
+        parseInput [] = []
+    cDesktops <- liftM parseInput $ readProcess "bspc" ["control", "--get-status"] ""
+    let split = map (\(s, f) -> let (a, b) = span (/= separator) s in (a, tail b, f)) cDesktops
+        sorted = sortBy (\(a, _, _) (b, _, _) -> a `compare` b) split
+        grouped = groupBy (\(a, _, _) (b, _, _) -> a == b) $ sorted
+        createWorkspace list@((a, _, _):s) =
+            let cIndex = findIndex (\(_, _, b) -> b) list
+            in (Workspace a (fromMaybe 0 cIndex) (map (\(_, d, _) -> d) list), isJust cIndex)
+        resList = map createWorkspace grouped
+    return $ UserState (fromJust $ findIndex snd resList) (map fst resList)
+
 loop :: UserState -> IO ()
 loop cState@(UserState wIdx wList) = do
     main:det <- getLine
@@ -38,7 +60,7 @@ loop cState@(UserState wIdx wList) = do
                             _ -> dIdx
                         resIdx = if newIdx >= length dList then dIdx else newIdx
                         newWState = Workspace wName resIdx dList
-                        newWList = take wIdx wList ++ newWState : drop (wIdx + 1) wList
+                        newWList = (wList `modInd` wIdx) newWState
                     in UserState wIdx newWList
                 'w' ->
                     let newIdx = case cmd of
@@ -63,47 +85,32 @@ loop cState@(UserState wIdx wList) = do
                     let Workspace wName dIdx dList = wList !! wIdx
                         newDIdx = fromMaybe dIdx $ elemIndex desktop dList
                         newWState = Workspace wName newDIdx dList
-                        newWList = take wIdx wList ++ newWState : drop (wIdx + 1) wList
-                    in UserState wIdx newWList
+                    in UserState wIdx (wList `modInd` wIdx) newWState
         'a' -> do
             let newState = case det of
                     "d" ->
                         let newDList = dList ++ [show $ length dList + 1]
                             newWState = Workspace wName (length dList) newDList
-                            newWList = take wIdx wList ++ newWState : drop (wIdx + 1) wList
-                        in UserState wIdx newWList
+                        in UserState wIdx (wList `modInd` wIdx) newWState
                     "w" ->
                         let newWList = wList ++ [Workspace (show $ length wList + 1) 0 ["1"]]
                         in UserState (length wList) newWList
+                    _ -> cState
             callProcess "bspc" ["monitor", "-a", getWorkspaceName newState]
             return newState
         'r' -> do
             let newState = case det of
                     'd':name ->
-                        let newDList = take dIdx dList ++ name : drop (dIdx + 1) dList
+                        let newDList = (dList `modInd` dIdx) name
                             newWState = Workspace wName dIdx newDList
-                            newWList = take wIdx wList ++ newWState : drop (wIdx + 1) wList
-                        in UserState wIdx newWList
+                        in UserState wIdx $ (wList `modInd` wIdx) newWState
                     'w':name ->
-                        let newWList = take wIdx wList ++ (Workspace name dIdx dList) : drop (wIdx + 1) wList
+                        let newWList = (wList `modInd` wIdx) $ Workspace name dIdx d List
                         in UserState wIdx newWList
+                    _ -> cState
             callProcess "bspc" ["desktop", "-n", getWorkspaceName newState]
             return newState
     callProcess "bspc" ["desktop", "-f", getWorkspaceName newState]
     loop newState
 
-main = do
-    let parseInput (':':c:name) = if toLower c `elem` "of"
-            then let (desktop, cont) = span (/= ':') name in (desktop, isUpper c) : parseInput cont
-            else parseInput name
-        parseInput (c:s) = parseInput s
-        parseInput [] = []
-    cDesktops <- liftM parseInput $ readProcess "bspc" ["control", "--get-status"] ""
-    let split = map (\(s, f) -> let (a, b) = span (/= separator) s in (a, tail b, f)) cDesktops
-        sorted = sortBy (\(a, _, _) (b, _, _) -> a `compare` b) split
-        grouped = groupBy (\(a, _, _) (b, _, _) -> a == b) $ sorted
-        createWorkspace list@((a, _, _):s) =
-            let cIndex = findIndex (\(_, _, b) -> b) list
-            in (Workspace a (fromMaybe 0 cIndex) (map (\(_, d, _) -> d) list), isJust cIndex)
-        resList = map createWorkspace grouped
-    loop $ UserState (fromJust $ findIndex snd resList) (map fst resList)
+main = readUserState >>= loop
